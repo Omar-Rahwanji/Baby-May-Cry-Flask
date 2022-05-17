676be387b31496688b2338513ebf372de76f6df5
Omar Rahwanji <rahwanjiomar@gmail.com>
2022-05-18 00:54:17 +0300

    Add ML model

--- app.py
+++ app.py
@@ -1,24 +1,33 @@
 import os
 import random
+import model_handler
 
 from flask import Flask, request
 
-cryReasons = ["Belly Pain", "Burping", "Discomfort", "Hungry", "Tired"]
+#cryReasons = ["Belly Pain", "Burping", "Discomfort", "Hungry", "Tired"]
+
 
 app = Flask(__name__)
+crySoundPath=None
 
 @app.route('/', methods=["POST","GET"])
 def cry_analysis():
+    global crySoundPath
     if request.method == "GET":
-        cryReason = random.choice(cryReasons)
+        #TODO Get last cry reason from db
+        print(crySoundPath)
+        cryReason = model_handler.makePrediction(crySoundPath)
         return {"cryReason": cryReason}
     else:
         if request.files:
             crySound = request.files['audio']
-            print(crySound)
-            crySound.save(os.path.join(crySound.filename))
-        return ""
 
+            crySoundPath=".\\"+os.path.join(crySound.filename)
+
+            crySound.save(crySoundPath)
+            #TODO Import the crySound to the ML model below
+            #TODO Store the cry reason in the db
+        return ""
 
 
 if __name__ == '__main__':
